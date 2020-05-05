46e7b4c6f6cb2d7bee850a128c3f2fd526f008b4
SiannaZhu <61836507+SiannaZhu@users.noreply.github.com>
2020-05-05 17:07:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 
 void set_up_new(string & difficulty,int & posibility){
-	cout<< "Please choose the difficulty level.(easy/normal/hard/expert)"<<endl;
+	cout << "Please choose the difficulty level.(easy/normal/hard/expert)"<<endl;
+	cin >> difficulty;
+	while (posibility == 0){
+		if (difficulty == "easy")
+			posibility = 30;
+		else if(difficulty == "normal")
+			posibility = 50;
+		else if(difficulty == "hard")
+			posibility = 80;
+		else if(difficulty == "expert")
+			posibility = 95;
+		else
+			cout << "Pleas input \"easy/normal/hard/expert\"! "<<endl;
+	}
 }
 
 
@@ -14,7 +27,7 @@ void set_up_new(string & difficulty,int & posibility){
 int main(){
 	cout<< "Start a new game?(y/n)"<<endl;
 	string command,mode,difficulty;
-	int posibility;
+	int posibility=0;
 	cin >> command;
 	if (command=="y"){
 		set_up_new();
