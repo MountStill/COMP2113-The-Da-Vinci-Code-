439afdbd9ae29d6126a060b3bb23b41d9b64d45a
MountStill <zcjiang@connect.hku.hk>
2020-05-05 23:10:21 +0800

    Update Da_Vinci_Code.cpp

--- Da_Vinci_Code.cpp
+++ Da_Vinci_Code.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 struct Player
 {
-    string palyername;
-    CardSet cardset;
+	string palyername;
+	CardSet cardset;
 };
 
 void SetDifficulty(double &prob_comp);
@@ -17,10 +17,10 @@ void SetDifficulty(double &prob_comp);
 
 int main()
 {
-    double prob_comp;
-    SetDifficulty(prob_comp);
-    CardPool cardpool;   
-    cardpool.Initialize();
+	double prob_comp;
+	SetDifficulty(prob_comp);
+	CardPool cardpool;   
+	cardpool.Initialize();
 
 
 
@@ -29,27 +29,27 @@ int main()
 
 void SetDifficulty(double &prob_comp)
 {
-    string difficulty_level;
-    ifstream initiator("Initiator.txt");
-    initiator >> difficulty_level;
-    if (difficulty_level == "EASY")
-    {
-        prob_comp = 0.1;
-    }
-    else if (difficulty_level == "NORMAL")
-    {
-        prob_comp = 0.3;
-    }
-    else if (difficulty_level == "HARD")
-    {
-        prob_comp = 0.5;
-    }
-    else if (difficulty_level == "EXPERT")
-    {
-        prob_comp = 0.8;
-    }
-    else if (difficulty_level == "HELL")
-    {
-        prob_comp = 1.0;
-    }
+	string difficulty_level;
+	ifstream initiator("Initiator.txt");
+	initiator >> difficulty_level;
+	if (difficulty_level == "EASY")
+	{
+		prob_comp = 0.1;
+	}
+	else if (difficulty_level == "NORMAL")
+	{
+		prob_comp = 0.3;
+	}
+	else if (difficulty_level == "HARD")
+	{
+		prob_comp = 0.5;
+	}
+	else if (difficulty_level == "EXPERT")
+	{
+		prob_comp = 0.8;
+	}
+	else if (difficulty_level == "HELL")
+	{
+		prob_comp = 1.0;
+	}
 }
