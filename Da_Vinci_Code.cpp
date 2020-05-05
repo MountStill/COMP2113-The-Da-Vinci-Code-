3c007d2666eacc2b6390726d89acc3e154a057ba
MountStill <jiangzc2000@163.com>
2020-05-05 22:53:46 +0800

    Add main function with the formal structure

---
+++ Da_Vinci_Code.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include "CardSet.h"
+#include "CardPool.h"
+using namespace std;
+
+struct Player
+{
+    string palyername;
+    CardSet cardset;
+};
+
+void SetDifficulty(double &prob_comp);
+
+
+int main()
+{
+    double prob_comp;
+    SetDifficulty(prob_comp);
+    CardPool cardpool;   
+    cardpool.Initialize();
+
+
+
+}
+
+
+void SetDifficulty(double &prob_comp)
+{
+    string difficulty_level;
+    ifstream initiator("Initiator.txt");
+    initiator >> difficulty_level;
+    if (difficulty_level == "EASY")
+    {
+        prob_comp = 0.1;
+    }
+    else if (difficulty_level == "NORMAL")
+    {
+        prob_comp = 0.3;
+    }
+    else if (difficulty_level == "HARD")
+    {
+        prob_comp = 0.5;
+    }
+    else if (difficulty_level == "EXPERT")
+    {
+        prob_comp = 0.8;
+    }
+    else if (difficulty_level == "HELL")
+    {
+        prob_comp = 1.0;
+    }
+}
