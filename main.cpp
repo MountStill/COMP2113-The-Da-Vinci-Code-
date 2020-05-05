b9610ee43552ab18c6e943b207979c2813dceb01
SiannaZhu <61836507+SiannaZhu@users.noreply.github.com>
2020-05-05 17:20:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,20 +17,25 @@ void set_up_new(string & difficulty,int & posibility){
 		else if(difficulty == "expert")
 			posibility = 95;
 		else
-			cout << "Pleas input \"easy/normal/hard/expert\"! "<<endl;
+			cout << "Please input \"easy/normal/hard/expert\"! "<<endl;
 	}
 }
 
-
+void load_game_process(player &p1,player &p2,player &pool,int &turn){
+}
 
 
 int main(){
 	cout<< "Start a new game?(y/n)"<<endl;
 	string command,mode,difficulty;
 	int posibility=0;
+	player p1,p2,pool;
+	int turn;
 	cin >> command;
 	if (command=="y"){
-		set_up_new();
+		set_up_new(difficulty,possibility);
 	}
+	else
+		load_game_process(p1,p2,pool,turn);
 
 }
