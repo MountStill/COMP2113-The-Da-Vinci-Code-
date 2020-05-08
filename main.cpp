6c83cde28823ec3da9f85b1ab693210c66c5e69c
MountStill <zcjiang@connect.hku.hk>
2020-05-08 23:46:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,14 +21,14 @@ void SetDifficulty(double& prob_comp);
 // choose the game difficulty.
 void LoadGameProgress(CardPool& cardpool, double& prob_comp, Player p[], int& turn);
 // input the progress of the last game.
-void GamePlay(CardPool& cardpool, double& prob_comp, Player p[], int& turn);
-// gameplaying.
-bool GameOver(Player p[], CardPool cardpool);
-// judge whether one of the player's card are all turned over.
 void OutputGameProgress(CardPool& cardpool, double& prob_comp, Player p[], int& turn);
 // output the progress into Progress.txt when the game is paused.
 void OutputSummary(Player p[], CardPool cardpool, int turn);
 // output the summary results of each player in the game.
+bool GameOver(Player p[], CardPool cardpool);
+// judge whether the game is ended.
+void GamePlay(CardPool& cardpool, double& prob_comp, Player p[], int& turn);
+// gameplaying.
 
 int main()
 {
@@ -59,15 +59,7 @@ int main()
 		}
 		if (command == "Y")
 		{
-			if (GameOver(p, cardpool))
-			{
-				OutputSummary(p, cardpool, turn);
-				break;
-			}
-			else
-			{
-				GamePlay(cardpool, probcomp, p, turn);
-			}
+			GamePlay(cardpool, probcomp, p, turn);
 		}
 		else if (command == "N")
 		{
@@ -89,38 +81,6 @@ void Introduction()
 		cout << line << endl;
 	}
 	introduction.close();
-}
-
-void SetDifficulty(double& probcomp)
-{
-	string difficultylevel;
-	cout << endl << "Please choose the difficulty level. (EASY/NORMAL/HARD/EXPERT/HELL)" << endl;
-	cin >> difficultylevel;
-	while (difficultylevel != "EASY" && difficultylevel != "NORMAL" && difficultylevel != "HARD" \
-	&& difficultylevel != "EXPERT" && difficultylevel != "HELL")
-	{
-		cout << "Please input \"EASY\"/\"NORMAL\"/\"HARD\"/\"EXPERT\"/\"HELL\"! " << endl;
-	}
-	if (difficultylevel == "EASY")
-	{
-		probcomp = 0.1;
-	}
-	else if (difficultylevel == "NORMAL")
-	{
-		probcomp = 0.3;
-	}
-	else if (difficultylevel == "HARD")
-	{
-		probcomp = 0.5;
-	}
-	else if (difficultylevel == "EXPERT")
-	{
-		probcomp = 0.8;
-	}
-	else if (difficultylevel == "HELL")
-	{
-		probcomp = 1.0;
-	}
 }
 
 void NewOrLoadGame(CardPool& cardpool, double& prob_comp, Player p[], int& turn)
@@ -158,6 +118,38 @@ void NewOrLoadGame(CardPool& cardpool, double& prob_comp, Player p[], int& turn)
 	}
 }
 
+void SetDifficulty(double& probcomp)
+{
+	string difficultylevel;
+	cout << endl << "Please choose the difficulty level. (EASY/NORMAL/HARD/EXPERT/HELL)" << endl;
+	cin >> difficultylevel;
+	while (difficultylevel != "EASY" && difficultylevel != "NORMAL" && difficultylevel != "HARD" \
+	&& difficultylevel != "EXPERT" && difficultylevel != "HELL")
+	{
+		cout << "Please input \"EASY\"/\"NORMAL\"/\"HARD\"/\"EXPERT\"/\"HELL\"! " << endl;
+	}
+	if (difficultylevel == "EASY")
+	{
+		probcomp = 0.1;
+	}
+	else if (difficultylevel == "NORMAL")
+	{
+		probcomp = 0.3;
+	}
+	else if (difficultylevel == "HARD")
+	{
+		probcomp = 0.5;
+	}
+	else if (difficultylevel == "EXPERT")
+	{
+		probcomp = 0.8;
+	}
+	else if (difficultylevel == "HELL")
+	{
+		probcomp = 1.0;
+	}
+}
+
 void LoadGameProgress(CardPool& cardpool, double& prob_comp, Player p[], int& turn)
 {
 	ifstream progress;
@@ -177,17 +169,6 @@ void LoadGameProgress(CardPool& cardpool, double& prob_comp, Player p[], int& tu
 	turn = stoi(s);
 }
 
-bool GameOver(Player p[], CardPool cardpool) {
-	if (p[0].cardset.NoHidden() || p[1].cardset.NoHidden() || cardpool.Empty())
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
-
 void OutputGameProgress(CardPool& cardpool, double& probcomp, Player p[], int& turn)
 {
 	ofstream progress;
@@ -200,87 +181,6 @@ void OutputGameProgress(CardPool& cardpool, double& probcomp, Player p[], int& t
 	progress << p[1].cardset.Archive() << endl;
 	progress << turn << endl;
 	progress.close();
-}
-
-void GamePlay(CardPool& cardpool, double& prob_comp, Player p[], int& turn) {
-	string newcard;
-	bool guesschance = 1;
-	newcard = cardpool.Pick();
-	string* guesscard = new string;
-	int* guessposition = new int;
-	if (turn % 2 == 1) {
-		cout << endl << "You get this card from the cardpool: ";
-		cout << newcard << endl;
-		while (guesschance)
-		{
-			cout << endl << "Please take a guess." << endl;
-			cout << endl << "You want to guess which card? (The leftmost position is 0.)" << endl;
-			cout << "Please enter the position:" << endl;
-			cin >> *guessposition;
-			while (*guessposition >= p[1].cardset.Num())
-			{
-				cout << "Please input an appropriate number! (The leftmost position is 0.)" << endl;
-				cin >> *guessposition;
-			}
-			cout << "You guess the card at that position should be: (In the form like \"01B\" or \"10W\")" << endl;
-			cin >> *guesscard;
-			if (p[1].cardset.Judge(*guessposition, *guesscard))
-			{
-				cout << endl << "Your guess is correct. The opponent card is turned over as reward." << endl;
-				p[1].cardset.TurnOver(*guessposition);
-				cout << endl << "Your view of your cardset and the opponent's cardset:" << endl << endl;
-				cout << setw(8) << p[0].playername << ": " << p[0].cardset.SecretDisplay() << endl;
-				cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
-				cout << endl << "Continue another guess or not? (Y/N)" << endl;
-				string command;
-				cin >> command;
-				while (command != "Y" && command != "N")
-				{
-					cout << "Please input \"Y\" or \"N\"." << endl;
-					cin >> command;
-				}
-				if (command == "N")
-				{
-					p[0].cardset.Append(newcard, 1);
-					guesschance = 0;
-				}
-			}
-			else
-			{
-				delete guesscard;
-				delete guessposition;
-				cout << endl << "Your guess is wrong. Your card just drawn is turned over as punishment." << endl;
-				p[0].cardset.Append(newcard, 0);
-				guesschance = 0;
-				cout << endl << "Your view of your cardset and the opponent's cardset:" << endl << endl;
-				cout << setw(8) << p[0].playername << ": " << p[0].cardset.SecretDisplay() << endl;
-				cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
-			}
-		}
-	}
-	else
-	{
-		cout << endl << "Computer gets one card from the cardpool." << endl;
-		srand((unsigned int)(time(0)));
-		if ((rand() % 10 + 1) <= (prob_comp * 10))
-		{
-			cout << endl << "The computer's guess is correct." << endl;
-			p[0].cardset.ForcedTurnOver();
-			p[1].cardset.Append(newcard, 1);
-			cout << endl << "The third view of two players' cardsets:" << endl << endl;
-			cout << setw(8) << p[0].playername << ": " << p[0].cardset.Display() << endl;
-			cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
-		}
-		else
-		{
-			cout << endl << "The computer's guess is wrong." << endl;
-			cout << endl << "The third view of two players' cardsets:" << endl << endl;
-			p[1].cardset.Append(newcard, 0);
-			cout << setw(8) << p[0].playername << ": " << p[0].cardset.Display() << endl;
-			cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
-		}
-	}
-	turn += 1;
 }
 
 void OutputSummary(Player p[], CardPool cardpool, int turn)
@@ -332,4 +232,106 @@ void OutputSummary(Player p[], CardPool cardpool, int turn)
 	cout << endl << "The summary of the game is output." << endl;
 	cout << endl << "You can find more details in the output file." << endl;
 	cout << endl << "Thanks for playing this game~" << endl;
+}
+
+bool GameOver(Player p[], CardPool cardpool) {
+	if (p[0].cardset.NoHidden() || p[1].cardset.NoHidden() || cardpool.Empty())
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void GamePlay(CardPool& cardpool, double& prob_comp, Player p[], int& turn) {
+	string newcard;
+	bool guesschance = 1;
+	newcard = cardpool.Pick();
+	string* guesscard = new string;
+	int* guessposition = new int;
+	if (turn % 2 == 1) {
+		cout << endl << "You get this card from the cardpool: ";
+		cout << newcard << endl;
+		while (guesschance)
+		{
+			cout << endl << "Please take a guess." << endl;
+			cout << endl << "You want to guess which card? (The leftmost position is 0.)" << endl;
+			cout << "Please enter the position:" << endl;
+			cin >> *guessposition;
+			while (*guessposition >= p[1].cardset.Num())
+			{
+				cout << "Please input an appropriate number! (The leftmost position is 0.)" << endl;
+				cin >> *guessposition;
+			}
+			cout << "You guess the card at that position should be: (In the form like \"01B\" or \"10W\")" << endl;
+			cin >> *guesscard;
+			if (p[1].cardset.Judge(*guessposition, *guesscard))
+			{
+				cout << endl << "Your guess is correct. The opponent card is turned over as reward." << endl;
+				p[1].cardset.TurnOver(*guessposition);
+				cout << endl << "Your view of your cardset and the opponent's cardset:" << endl << endl;
+				cout << setw(8) << p[0].playername << ": " << p[0].cardset.SecretDisplay() << endl;
+				cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
+				if (GameOver(p, cardpool))
+				{
+					OutputSummary(p, cardpool, turn);
+					exit(0);
+				}
+				cout << endl << "Continue another guess or not? (Y/N)" << endl;
+				string command;
+				cin >> command;
+				while (command != "Y" && command != "N")
+				{
+					cout << "Please input \"Y\" or \"N\"." << endl;
+					cin >> command;
+				}
+				if (command == "N")
+				{
+					p[0].cardset.Append(newcard, 1);
+					guesschance = 0;
+				}
+			}
+			else
+			{
+				delete guesscard;
+				delete guessposition;
+				cout << endl << "Your guess is wrong. Your card just drawn is turned over as punishment." << endl;
+				p[0].cardset.Append(newcard, 0);
+				guesschance = 0;
+				cout << endl << "Your view of your cardset and the opponent's cardset:" << endl << endl;
+				cout << setw(8) << p[0].playername << ": " << p[0].cardset.SecretDisplay() << endl;
+				cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << endl << "Computer gets one card from the cardpool." << endl;
+		srand((unsigned int)(time(0)));
+		if ((rand() % 10 + 1) <= (prob_comp * 10))
+		{
+			cout << endl << "The computer's guess is correct." << endl;
+			p[0].cardset.ForcedTurnOver();
+			p[1].cardset.Append(newcard, 1);
+			cout << endl << "The third view of two players' cardsets:" << endl << endl;
+			cout << setw(8) << p[0].playername << ": " << p[0].cardset.Display() << endl;
+			cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
+			if (GameOver(p, cardpool))
+			{
+				OutputSummary(p, cardpool, turn);
+				exit(0);
+			}
+		}
+		else
+		{
+			cout << endl << "The computer's guess is wrong." << endl;
+			cout << endl << "The third view of two players' cardsets:" << endl << endl;
+			p[1].cardset.Append(newcard, 0);
+			cout << setw(8) << p[0].playername << ": " << p[0].cardset.Display() << endl;
+			cout << setw(8) << p[1].playername << ": " << p[1].cardset.Display() << endl;
+		}
+	}
+	turn += 1;
 }
