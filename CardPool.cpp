532e2a63d4ceeff18593ebc2546436246238d9f5
MountStill <zcjiang@connect.hku.hk>
2020-05-08 17:55:54 +0800

    Update CardPool.cpp

--- CardPool.cpp
+++ CardPool.cpp
@@ -8,11 +8,23 @@ using namespace std;
 
 
 void CardPool::Initialize()
- {
+{
 	cardpool.clear();
-	cardpool = {"00B", "01B", "02B", "03B", "04B", "05B", "06B", "07B", "08B", \
+	cardpool = { "00B", "01B", "02B", "03B", "04B", "05B", "06B", "07B", "08B", \
 	"09B", "10B", "00W", "01W", "02W", "03W", "04W", "05W", "06W", "07W", "08W", \
-	"09W", "10W"};
+	"09W", "10W" };
+}
+
+bool CardPool::Empty()
+{
+	if (cardpool.size() == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 string CardPool::Pick()
@@ -35,13 +47,23 @@ string CardPool::Archive()
 	return archive;
 }
 
-void CardPool::Input(string s)
+void CardPool::Load(string s)
 {
 	istringstream iss(s);
 	string card;
 	cardpool.clear();
-	while (iss>> card)
+	while (iss >> card)
 	{
 		cardpool.push_back(card);
 	}
+}
+
+string CardPool::Display()
+{
+	string display;
+	for (int i = 0; i < cardpool.size(); i++)
+	{
+		display += (cardpool[i] + " ");
+	}
+	return display;
 }
