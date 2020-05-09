1d71874ac298293c6d187b02ed691ab812187d61
SiannaZhu <61836507+SiannaZhu@users.noreply.github.com>
2020-05-09 13:45:26 +0800

    Add comment on the functions

--- CardPool.h
+++ CardPool.h
@@ -8,11 +8,15 @@ class CardPool
 {
 public:
 	void Initialize();
+//Initialize the cardpool with all cards placed in it at the beginning.
 	bool Empty();
+//Judge whether the vector is empty.
 	std::string Pick();
+// Randomly pick a card from the carpool and output the card as a string. 
 	std::string Archive();
+//Display the cardpool as a string and it can be used as the cardpool archive.
 	void Load(std::string);
-	std::string Display();
+//Store the data received from the string into cardpool. 
 private:
 	std::vector<std::string> cardpool;
 };
