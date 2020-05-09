c78d17dad1c111b27a893d053977feaf6c0aa568
SiannaZhu <61836507+SiannaZhu@users.noreply.github.com>
2020-05-09 14:23:48 +0800

    Add card "11B" &"11W"

--- CardPool.cpp
+++ CardPool.cpp
@@ -11,8 +11,8 @@ void CardPool::Initialize()
 {
 	cardpool.clear();
 	cardpool = { "00B", "01B", "02B", "03B", "04B", "05B", "06B", "07B", "08B", \
-	"09B", "10B", "00W", "01W", "02W", "03W", "04W", "05W", "06W", "07W", "08W", \
-	"09W", "10W" };
+	"09B", "10B", "11B", "00W", "01W", "02W", "03W", "04W", "05W", "06W", "07W", "08W", \
+	"09W", "10W", "11W" };
 }
 
 bool CardPool::Empty()
